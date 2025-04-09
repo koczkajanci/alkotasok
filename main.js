755af903f204387649862f1a87c7dd0c4ef4c7e9
Buttyan Peter <peti2132@gmail.com>
2025-04-09 21:06:00 +0200

    tablazat hozzadasa sima

--- main.js
+++ main.js
@@ -12,6 +12,20 @@ const divMaker = (className) => {  //Letrehozzuk a divMaker fuggvenyt ami a clas
 const containerDiv = divMaker('container'); //containerDiv letrehozasa a divMakerrel
 document.body.appendChild(containerDiv);//Hozzaadjuk a bodyhoz a containerDivet
 const tableDiv = divMaker('table');//tableDiv letrehozasa a divMakerrel
+const tableElement = document.createElement('table') // Letrehozunk egy table elemet
+ tableDiv.appendChild(tableElement) // Hozzaadjuk a tableDivhez
+ const tableHeader = document.createElement('thead') // Letrehozunk egy thead elemet
+ tableElement.appendChild(tableHeader) // Hozzaadjuk a tablehez
+ const headerRow = document.createElement('tr') // Letrehozunk egy tr elemet
+ tableHeader.appendChild(headerRow) // Hozzaadjuk a theadhez
+ const headerCells = ['Szerzo', 'mufaj', 'cim'] // A fejlec cellainak tartalma
+ for (const cellText of headerCells) {
+     const headerCell = document.createElement('th') // Letrehozunk egy th elemet
+    headerCell.innerText = cellText // Beallitjuk a cella szoveget
+    headerRow.appendChild(headerCell) // Hozzaadjuk a sorhoz
+ }
+ const tableBody = document.createElement('tbody') // Letrehozunk egy tbody elemet
+ tableElement.appendChild(tableBody) // Hozzaadjuk a tablehez
 
 const formDiv = divMaker('form');//formDiv letrehozasa a divMakerrel
 
