a79e391bf173e2244aced84aa221feb7693c915b
Buttyan Peter <peti2132@gmail.com>
2025-04-13 21:16:06 +0200

    form letrehozasa oop

--- oop/area.js
+++ oop/area.js
@@ -59,3 +59,46 @@ class Table extends Area{
         tableElement.appendChild(tableBody); // Hozzaadjuk a tablehez
     }
 }
+/**
+ * Ez az ostaly az Area osztalybol szarmazik es letrehoz egy űrlapot mezokkel es egy gombbal 
+ */
+class Form extends Area{
+    /**
+     * Ez a konstruktor létrehoz egy űrlapot mezőkkel és hozzáadja a szülő div elemhez.
+     * @param {string} className A div osztalyneve ami stringet var
+     */
+    constructor(className){// //Konstruktor letrehozasa
+        super(className);//Meghivjuk a szulo osztaly konstruktorat
+        const formElement = document.createElement('form'); // Letrehozunk egy form elemet
+        this.div.appendChild(formElement); // Hozzaadjuk a divhez
+        const fieldElements = [{//Letrehozzuk a fieldElements tombot
+            id: 'writer', // Elso mezo idje
+            label: 'Szerzo', // Elso mezo labelje
+        },
+        {
+            id: 'genre', // Masodik mezo idje
+            label: 'mufaj', // Masodik mezo labelje
+        }, 
+        {
+            id: 'title', // Harmadik mezo idje
+            label: 'cim', // Harmadik mezo labelje
+        }]; 
+        for(const element of fieldElements){ //Vegigmegyunk a fieldElements tombon az element valtozoval
+            const field = document.createElement('div'); // Letrehozunk egy div elemet a mezokhoz
+            formElement.appendChild(field); // Hozzaadjuk a formhoz a fieldet
+            const label = document.createElement('label'); // Letrehozunk egy label elemet
+            label.htmlFor = element.id; // Beallitjuk a label htmlForjat az element idjere
+            label.textContent = element.label; // Beallitjuk a label szoveget az element labeljere
+            field.appendChild(label); // Hozzaadjuk a fieldhez a labelt
+            const input = document.createElement('input'); // Letrehozunk egy input elemet
+            input.id = element.id; // Beallitjuk az input idjet az element idjere
+            field.appendChild(document.createElement('br')); // Hozzaadunk egy sortorest a fieldhez
+            field.appendChild(input); // Hozzaadjuk a fieldhez az inputot
+        }   
+
+        const button = document.createElement('button'); //Letrehozzuk a gombot
+        button.textContent = 'hozzáadás'; //Beallitjuk a gomb szoveget
+        formElement.appendChild(button); //Hozzaadjuk a formhoz a gombot
+
+    }
+}
--- oop/main.js
+++ oop/main.js
@@ -1,4 +1,4 @@
 const separator = document.createElement("hr"); //Uj elem letrehozasa
 document.body.appendChild(separator); //Hozzaadjuk a bodyhoz a separator elemet
 const table = new Table('table'); // Uj Tablet hozunk letre table parameterrel
-const form = new Area('form'); // Uj Areat hozunk letre form parameterrel
+const form = new Form('form'); // Uj Formot hozunk letre form parameterrel
